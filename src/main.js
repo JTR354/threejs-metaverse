830b1688d6b856b50abe943a658bfcc3d93b0136
jtr354 <jtr354@foxmail.com>
2023-03-15 15:56:40 +0800

    feat: hello threejs

--- src/main.js
+++ src/main.js
@@ -1,5 +1,31 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import * as THREE from "three";
 
-createApp(App).mount('#app')
+/**
+ * 1. 场景 scene
+ * 2. 相机 camera
+ * 3. 模型 mesh
+ * 4. 灯光 light
+ * 5. 渲染器 renderer
+ */
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(
+  45,
+  window.innerWidth / window.innerHeight,
+  0.01,
+  50
+);
+const renderer = new THREE.WebGLRenderer();
+
+const boxGeometry = new THREE.BoxGeometry(1, 1, 1);
+const boxMaterial = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const boxMesh = new THREE.Mesh(boxGeometry, boxMaterial);
+
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+scene.add(boxMesh);
+
+camera.position.set(0, 0, 5);
+
+document.body.appendChild(renderer.domElement);
+renderer.render(scene, camera);
